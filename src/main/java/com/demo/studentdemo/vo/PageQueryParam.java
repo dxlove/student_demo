a19f070e4548709482deba72fd1f339233ba4ae4
dxlove <duxu111@gmail.com>
2025-08-03 19:53:28 +0800

    feat:更新sw

--- src/main/java/com/demo/studentdemo/vo/PageQueryParam.java
+++ src/main/java/com/demo/studentdemo/vo/PageQueryParam.java
@@ -1,5 +1,6 @@
 package com.demo.studentdemo.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -9,10 +10,12 @@ import org.hibernate.validator.constraints.Range;
 public class PageQueryParam<T> {
     @NotNull(message = "页码不能为空")
     @Min(value = 1, message = "页码最小为1")
+    @Schema(description = "页码", example = "1")
     private Integer pageNum = 1;
 
     @NotNull(message = "每页数量不能为空")
     @Range(min = 1, max = 200, message = "每页数量在1-200之间")
+    @Schema(description = "每页数量", example = "10")
     private Integer pageSize = 10;
 
     private T condition; // 通用查询条件
--- src/main/java/com/demo/studentdemo/vo/StudentGradeVO.java
+++ src/main/java/com/demo/studentdemo/vo/StudentGradeVO.java
@@ -1,9 +1,11 @@
 package com.demo.studentdemo.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class StudentGradeVO {
+    @Schema(description = "学号", example = "202103210001")
     private String studentId;
     private String studentName;
     private String department;
